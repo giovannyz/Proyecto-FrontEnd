58e17e80f50abadf44b4327bbb3c91c981cb2fef
giovannyz <guiovanyhz06@gmail.com>
2022-11-19 14:38:41 -0500

    Pagos, new java

---
+++ DatosHamburguesa.js
@@ -0,0 +1,33 @@
+/*ARRAY DE OBJETOS CON DATOS DE HAMBURGUESA*/
+
+const hamburguesa=[{
+    name:"hamburguesa 1",
+    precio:15,
+  
+  },{
+    name:"hamburguesa 2",
+    precio:20
+  },{
+    name:"hamburguesa 3",
+    precio:20
+  },{
+    name:"hamburguesa 4",
+    precio:20
+  },{
+    name:"hamburguesa 5",
+    precio:20
+  },{
+    name:"hamburguesa 6",
+    precio:20
+  },{
+    name:"hamburguesa 7",
+    precio:20
+  },{
+    name:"hamburguesa 8",
+    precio:20
+  },{
+    name:"hamburguesa 9",
+    precio:20
+  }
+  ]
+  console.log(hamburguesa)
--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const btnConfirmar = document.querySelector("#btn-confirmar")
 const btnCancelar = document.querySelector("#btn-cancelar")
 
+/*BOTONES CARTA*/
+const btnAgregar1=document.querySelector("#agregar1")
+const btnAgregar2=document.querySelector("#agregar2")
+const btnAgregar3=document.querySelector("#agregar3")
+const btnAgregar4=document.querySelector("#agregar4")
+const btnAgregar5=document.querySelector("#agregar5")
+const btnAgregar6=document.querySelector("#agregar6")
+const btnAgregar7=document.querySelector("#agregar7")
+const btnAgregar8=document.querySelector("#agregar8")
+const btnAgregar9=document.querySelector("#agregar9")
+
+/*BOTONES PROMOCION*/
+
+const btnPromocion1=document.querySelector("#comprar1")
+const btnPromocion2=document.querySelector("#comprar2")
+const btnPromocion3=document.querySelector("#comprar3")
+const btnPromocion4=document.querySelector("#comprar4")
+const btnPromocion5=document.querySelector("#comprar5")
+const btnPromocion6=document.querySelector("#comprar6")
+
+/* boton */
+
+const hamburguesa=[{
+  name:"papas",
+  precio:15,
+
+},{
+  name:"chorizo",
+  precio:20
+}
+]
+console.log(hamburguesa[0])
 
 btnConfirmar.addEventListener("click",function(){
     
